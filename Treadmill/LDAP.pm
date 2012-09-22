141497f495a83880bfb78735b8129031a1cc9d8b
Mark Warren <mwarren@symas.com>
2012-09-21 22:26:55 -0600

    New functions, perldoc, & constants. See extended.
    - Added subs for reading in LDIF files
    - Added some error code constants for LDIF. Added a function to turn them into
      generic error messages.
    - function to get x number of results from a Net::LDAP::Search result object.
    - Added perldoc
    - Spelling fixes

--- Treadmill/LDAP.pm
+++ Treadmill/LDAP.pm
@@ -30,6 +30,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # 
 # -----------------------------------------------------------------------
+
 package Treadmill::LDAP;
 
 use strict;
@@ -37,15 +38,41 @@ use warnings;
 
 
 use Net::LDAP;
+use Net::LDAP::LDIF;
 use Net::LDAP::Util qw( ldap_error_name ldap_error_text ldap_error_desc );
 
 require Exporter;
 
-our $VERSION		= .1;
+our $VERSION		= 0.2;
 our @ISA			= qw( Exporter );
 
-our @EXPORT_OK		= qw( LDAP_Connect LDAP_Close LDAP_Fail_Msg);
+our @EXPORT_OK		= qw( LDAP_Connect LDAP_Close LDAP_Fail_Msg LDIF_Open 
+						LDAP_Get_Set LDIF_Get_Set LDIF_Close LDIF_Err_Text );
 #our %EXPORT_TAGS	= ();
+
+#############
+# Constants #
+#############
+
+use constant LDIF_SUCCESS     => 0;
+
+use constant LDIF_ERR_NOEXIST	=> 1;
+use constant LDIF_ERR_NOREAD	=> 2;
+use constant LDIF_ERR_PARSE		=> 3;
+use constant LDIF_ERR_NOTFILE	=> 4;
+use constant LDIF_ERR_OPEN		=> 5;
+
+#########
+# Other #
+#########
+
+my %LDIF_Err_Msgs = (
+	LDIF_ERR_NOEXIST	=> 'The specified LDIF file does not exist.',
+	LDIF_ERR_NOREAD		=> 'The specified LDIF file is not readable.',
+	LDIF_ERR_PARSE		=> 'There was an issue parsing the LDIF file.',
+	LDIF_ERR_NOTFILE	=> 'The path specified does not point to a file.',
+	LDIF_ERR_OPEN		=> 'There was an error opening the specified LDIF file',
+);
 
 
 #############
@@ -53,6 +80,7 @@ our @EXPORT_OK		= qw( LDAP_Connect LDAP_Close LDAP_Fail_Msg);
 #############
 
 =head1 FUNCTIONS
+
 =cut
 
 =head LDAP_Connect
@@ -69,7 +97,7 @@ sub LDAP_Connect {
 	my $ldap;
 	my $msg;	
 
-	# Determine if we're using tls and connect appropriately
+	# Determine if we're using TLS and connect appropriately
 	if ( $server =~ m/^ldaps:\/\//i ) {
 
 		# test for required modules
@@ -121,7 +149,8 @@ sub LDAP_Close {
 }
 
 =head2 LDAP_Fail_Msg
- Purpose: Create an informative and verbose  error message from an LDAP error code.
+
+Purpose: Create an informative and verbose  error message from an LDAP error code.
  Accepts: LDAP error code
  Returns: Error string
 
@@ -145,6 +174,120 @@ sub LDAP_Fail_Msg {
 	return $str;
 }
 
+
+=head2 LDAP_Get_Set
+
+Purpose: Get N Net::LDAP::Entry objects and return them in an array.
+ Accepts: Net::LDAP::Search object and a count.
+ Returns: An array of references or undef if empty
+
+ Note: This subroutine will block while new entries come in from LDAP.
+ Should not be terrible most of the time, though, since openldap is 
+ much faster than perl. 
+
+=cut
+
+sub LDAP_Get_Set {
+	my $search = shift;
+	my $count = shift;
+	my @arr;
+	
+	my $entry_ref;
+	while ($count--) {
+		$entry_ref = \$search->shift_entry();
+		last unless (defined(${$entry_ref}));
+		push(@arr, $entry_ref);
+	}
+	return \@arr, undef;
+}
+
+=head2 LDIF_Open
+
+Purpose: Open an ldif file and returnan ldif object. 
+ Accepts: The LDIF file location.
+ Returns: An ldif object of undef & an error code.
+
+=cut
+
+#TODO: TEST
+sub LDIF_Open {
+	my $file = shift;
+	return undef, LDIF_ERR_NOEXIST    unless (-e $file);
+	return undef, LDIF_ERR_NOREAD     unless (-r $file);
+	return undef, LDIF_ERR_NOTFILE    unless (-f $file || -l $file);
+
+	my $ldif = Net::LDAP::LDIF->new($file, 'r', onerror => undef, wrap => 0);
+
+	return undef, LDIF_ERR_OPEN unless (defined($ldif));
+	
+	return $ldif, LDIF_SUCCESS;
+}
+
+
+=head2 LDIF_Get_Set 
+
+ Purpose: Get N Net::LDAP::Entry objects and return them in an array.
+ Accepts: An LDIF object and a count.
+ Returns: An array of references or undef if empty.
+
+=cut
+
+#TODO: TEST
+sub LDIF_Get_Set {
+	my $ldif = shift;
+	my $count = shift;
+	my @arr = ();
+	my $err;
+
+	my $entry_ref;
+	while ($count-- && !$ldif->eof()) {
+		$entry_ref = \$ldif->read_entry();
+		unless (${$entry_ref}) {
+			$err  = "\nLDIF error: ";
+			$err .= $ldif->error() . "\n";
+			$err .= "Halted on following text:\n\n";
+			$err .= $ldif->error_lines ."\n\n";
+
+			return undef, $err;
+			#die("\n" . $ldif->error . "\nLines: " . $ldif->error_lines . "\n\n");
+		}
+
+		push(@arr, $entry_ref);
+
+	}
+
+	return \@arr, undef;
+}
+
+
+=head2 LDIF_Close
+
+ Purpose: Close an LDAP conection
+ Accepts: An LDAP connection
+ Returns: Nothing
+
+=cut
+ 
+sub LDIF_Close {
+	my $ldif = shift;
+	$ldif->done();
+}
+
+
+=head2 LDIF_Err_Text
+
+ Purpose: Retrieve a generic error message for a given treadmill LDIF file 
+          error code. 
+ Accepts: An error code.
+ Returns: A string description. undef on failure. 
+
+=cut
+
+sub LDIF_err_Text {
+	my $err = shift;
+	return $LDIF_Err_Msgs{$err} if (exists($LDIF_Err_Msgs{$err}));
+	return undef;
+}
 
 
 
